322eb1fe8cdb241f511ff8f4efbf05db51104624
Munis <will_smith94@mail.ru>
2015-03-17 03:13:10 +0500

    saytning yangi korinishi Uzbekistan 17 03  15

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -12,5 +12,37 @@ object Application extends Controller {
   def andijon = Action{
     Ok(views.html.andijon())
   }
-
+  def bukhara = Action{
+    Ok(views.html.bukhara())
+  }
+  def jizzax = Action{
+    Ok(views.html.jizzax())
+  }
+  def qashqadaryo = Action{
+    Ok(views.html.qashqadaryo())
+  }
+  def navoiy = Action{
+    Ok(views.html.navoiy())
+  }
+  def namangan = Action{
+    Ok(views.html.namangan())
+  }
+  def samarqand = Action{
+    Ok(views.html.samarqand())
+  }
+  def surxondaryo = Action{
+    Ok(views.html.surxondaryo())
+  }
+  def sirdaryo = Action{
+    Ok(views.html.sirdaryo())
+  }
+  def tashkent = Action{
+    Ok(views.html.tashkent())
+  }
+  def farghana = Action{
+    Ok(views.html.farghana())
+  }
+  def khorezm = Action{
+    Ok(views.html.khorezm())
+  }
 }
